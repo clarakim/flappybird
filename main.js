a43b646ff91a3c54c00adf21926ad798153c8cf6
Clara Kim <clara.kim@43344.local>
2014-04-08 14:28:04 +1200

    angry bird icon

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var main_state = {
      this.game.stage.backgroundColor = '#71c5cf'; 
 
      // Load the bird sprite
-     this.game.load.image ('bird', 'assets/bird.png');
+     this.game.load.image ('bird', 'assets/angry-bird-icon.png');
 
      // Load the pipe sprite 
      this.game.load.image('pipe', 'assets/pipe.png');
